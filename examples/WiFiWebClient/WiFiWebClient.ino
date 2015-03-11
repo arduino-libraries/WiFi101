54b6e6bd885fe38cef6ee2873bc26cd9d751ee78
Cristian Maglie <c.maglie@arduino.cc>
2015-03-11 11:11:39 +0100

    Updated examples from latest WiFi library

--- examples/WiFiWebClient/WiFiWebClient.ino
+++ examples/WiFiWebClient/WiFiWebClient.ino
@@ -1,20 +1,21 @@
 
 /*
   Web client
- 
+
  This sketch connects to a website (http://www.google.com)
  using a WiFi shield.
- 
- This example is written for a network using WPA encryption. For 
+
+ This example is written for a network using WPA encryption. For
  WEP or WPA, change the Wifi.begin() call accordingly.
- 
- This example is written for a network using WPA encryption. For 
+
+ This example is written for a network using WPA encryption. For
  WEP or WPA, change the Wifi.begin() call accordingly.
- 
+
  Circuit:
  * WiFi shield attached
- 
+
  created 13 July 2010
+ by dlf (Metodo2 srl)
  modified 31 May 2012
  by Tom Igoe
  */
@@ -23,62 +24,66 @@
 #include <SPI.h>
 #include <WiFi101.h>
 
-char ssid[] = "yourNetwork"; //  your network SSID (name) 
+char ssid[] = "yourNetwork"; //  your network SSID (name)
 char pass[] = "secretPassword";    // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0;            // your network key Index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
-IPAddress server(173,194,73,105);  // numeric IP for Google (no DNS)
-//char server[] = "www.google.com";    // name address for Google (using DNS)
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+char server[] = "www.google.com";    // name address for Google (using DNS)
 
 // Initialize the Ethernet client library
-// with the IP address and port of the server 
+// with the IP address and port of the server
 // that you want to connect to (port 80 is default for HTTP):
 WiFiClient client;
 
 void setup() {
   //Initialize serial and wait for port to open:
-  Serial.begin(9600); 
+  Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-  
+
   // check for the presence of the shield:
   if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present"); 
+    Serial.println("WiFi shield not present");
     // don't continue:
-    while(true);
-  } 
-  
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if ( fv != "1.1.0" )
+    Serial.println("Please upgrade the firmware");
+
   // attempt to connect to Wifi network:
-  while ( status != WL_CONNECTED) { 
+  while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     status = WiFi.begin(ssid, pass);
-  
+
     // wait 10 seconds for connection:
     delay(10000);
-  } 
+  }
   Serial.println("Connected to wifi");
   printWifiStatus();
-  
+
   Serial.println("\nStarting connection to server...");
   // if you get a connection, report back via serial:
   if (client.connect(server, 80)) {
     Serial.println("connected to server");
     // Make a HTTP request:
     client.println("GET /search?q=arduino HTTP/1.1");
-    client.println("Host:www.google.com");
+    client.println("Host: www.google.com");
     client.println("Connection: close");
     client.println();
   }
 }
 
 void loop() {
-  // if there are incoming bytes available 
+  // if there are incoming bytes available
   // from the server, read them and print them:
   while (client.available()) {
     char c = client.read();
@@ -92,7 +97,7 @@ void loop() {
     client.stop();
 
     // do nothing forevermore:
-    while(true);
+    while (true);
   }
 }
 
--- examples/WiFiWebServer/WiFiWebServer.ino
+++ examples/WiFiWebServer/WiFiWebServer.ino
@@ -1,25 +1,28 @@
 /*
-  Web  Server
- 
+  WiFi Web Server
+
  A simple web server that shows the value of the analog input pins.
  using a WiFi shield.
- 
- This example is written for a network using WPA encryption. For 
+
+ This example is written for a network using WPA encryption. For
  WEP or WPA, change the Wifi.begin() call accordingly.
- 
+
  Circuit:
  * WiFi shield attached
  * Analog inputs attached to pins A0 through A5 (optional)
- 
+
  created 13 July 2010
+ by dlf (Metodo2 srl)
  modified 31 May 2012
  by Tom Igoe
+
  */
+
 #include <SPI.h>
 #include <WiFi101.h>
 
 
-char ssid[] = "yourNetwork";      //  your network SSID (name) 
+char ssid[] = "yourNetwork";      // your network SSID (name)
 char pass[] = "secretPassword";   // your network password
 int keyIndex = 0;                 // your network key Index number (needed only for WEP)
 
@@ -29,28 +32,32 @@ WiFiServer server(80);
 
 void setup() {
   //Initialize serial and wait for port to open:
-  Serial.begin(9600); 
+  Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-  
+
   // check for the presence of the shield:
   if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present"); 
+    Serial.println("WiFi shield not present");
     // don't continue:
-    while(true);
-  } 
-  
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if ( fv != "1.1.0" )
+    Serial.println("Please upgrade the firmware");
+
   // attempt to connect to Wifi network:
-  while ( status != WL_CONNECTED) { 
+  while ( status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     status = WiFi.begin(ssid, pass);
 
     // wait 10 seconds for connection:
     delay(10000);
-  } 
+  }
   server.begin();
   // you're connected now, so print out the status:
   printWifiStatus();
@@ -75,12 +82,11 @@ void loop() {
           // send a standard http response header
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
-          client.println("Connnection: close");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
           client.println();
           client.println("<!DOCTYPE HTML>");
           client.println("<html>");
-          // add a meta refresh tag, so the browser pulls again every 5 seconds:
-          client.println("<meta http-equiv=\"refresh\" content=\"5\">");
           // output the value of each analog input pin
           for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
             int sensorReading = analogRead(analogChannel);
@@ -88,15 +94,15 @@ void loop() {
             client.print(analogChannel);
             client.print(" is ");
             client.print(sensorReading);
-            client.println("<br />");       
+            client.println("<br />");
           }
           client.println("</html>");
-           break;
+          break;
         }
         if (c == '\n') {
           // you're starting a new line
           currentLineIsBlank = true;
-        } 
+        }
         else if (c != '\r') {
           // you've gotten a character on the current line
           currentLineIsBlank = false;
@@ -105,9 +111,10 @@ void loop() {
     }
     // give the web browser time to receive the data
     delay(1);
-      // close the connection:
-      client.stop();
-      Serial.println("client disonnected");
+
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
   }
 }
 
