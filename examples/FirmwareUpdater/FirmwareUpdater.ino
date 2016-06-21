0892e61d2c6b1078c51309320449405a52d44047
agdl <a.guadalupi@arduino.cc>
2016-06-21 15:19:11 +0200

    Added some reference as required in #69

--- examples/FirmwareUpdater/FirmwareUpdater.ino
+++ examples/FirmwareUpdater/FirmwareUpdater.ino
@@ -2,19 +2,12 @@
   FirmwareUpdate.h - Firmware Updater for WiFi101 / WINC1500.
   Copyright (c) 2015 Arduino LLC.  All right reserved.
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  This sketch is used to upgrade the firmware present on an WINC1500 module
+  (i.e WiFi Shield 101 MKR1000)
+  
+  Once this sketch has been uploaded please refer to the instructions you can
+  find here https://github.com/arduino-libraries/WiFi101-FirmwareUpdater
+  to upgrade your board
 */
 
 #include <WiFi101.h>
